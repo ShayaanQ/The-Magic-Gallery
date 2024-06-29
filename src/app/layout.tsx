828576f4e8349ad5d161b74999d16cd2756658e0
Shayaan Qureshi <136394273+ShayaanQ@users.noreply.github.com>
2024-06-29 14:30:49 -0500

    running

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import "~/styles/globals.css";
+import "@uploadthing/react/styles.css";
+
+import { ClerkProvider } from '@clerk/nextjs'
+
+
 import { Inter } from "next/font/google";
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { TopNav } from "./_components/topnav";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 
 
@@ -15,22 +23,6 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-function TopNav() {
-  return (
-    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div >Gallery</div>
-      <div >
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </div>
-    </nav>
-  );
-}
-
 export default function RootLayout({
   children,
 }: {
@@ -39,6 +31,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
+    <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
       <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
         <TopNav/>
         {children}
