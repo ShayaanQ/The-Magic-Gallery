e7b4271c095e16fa1a1ae31f5bf7200a44223893
Shayaan Qureshi <136394273+ShayaanQ@users.noreply.github.com>
2024-07-02 19:55:54 -0500

    test

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { Modal } from "./modal";
 import { FullPageImageView } from "~/common/full-image-page";
 
-export async function PhotoModal({
+export default async function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
